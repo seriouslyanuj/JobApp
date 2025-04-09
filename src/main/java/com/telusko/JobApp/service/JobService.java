6ef73ab27d19c861f7909b815b309aabc75f0653
Anuj Gupta <106159652+seriouslyanuj@users.noreply.github.com>
2025-04-09 21:20:13 +0530

    Initial commit

---
+++ src/main/java/com/telusko/JobApp/service/JobService.java
@@ -0,0 +1,22 @@
+package com.telusko.JobApp.service;
+
+import com.telusko.JobApp.model.JobPost;
+import com.telusko.JobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost){
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs(){
+        return repo.getAllJobs();
+    }
+}
